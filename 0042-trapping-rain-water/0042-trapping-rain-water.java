8a7ea05c8a00fb47e5810ddeb9517226398ea1b3
Chaitanya Changala <160673221+CChangala@users.noreply.github.com>
2025-10-10 16:02:44 -0500

    Time: 0 ms (100%), Space: 46.7 MB (21.64%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int trap(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        int water = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                // Left side is the limiting wall
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    water += leftMax - height[left];
+                }
+                left++;
+            } else {
+                // Right side is the limiting wall
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    water += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+        return water;
+
+        
+    }
+}
