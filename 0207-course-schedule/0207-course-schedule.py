502677bd532f7ff875cc7fceae1d768a65262e1f
Chaitanya Changala <160673221+CChangala@users.noreply.github.com>
2026-02-19 14:34:18 -0500

    Time: 7 ms (45.76%), Space: 20.4 MB (62.11%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,34 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        adj = [[] for _ in range(numCourses)]
+    
+        # Build indegree array
+        indegree = [0] * numCourses
+        
+        for course, prereq in prerequisites:
+            adj[prereq].append(course)
+            indegree[course] += 1
+
+        #queue
+        queue = deque()
+
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                queue.append(i)
+        
+        processed = 0
+
+        while queue:
+            node = queue.popleft()
+            processed += 1
+            for neighbors in adj[node]:
+                indegree[neighbors] -= 1
+                if indegree[neighbors] == 0:
+                    queue.append(neighbors)
+        
+        return processed == numCourses
+            
+
+
+        
